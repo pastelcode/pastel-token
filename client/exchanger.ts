a97ec9cab34a5d1629feae89513cd0fcd88d4372
Pastel <pastel.dev.gt@gmail.com>
2022-11-03 16:47:03 -0600

    Add functionality to sell tokens

--- client/exchanger.ts
+++ client/exchanger.ts
@@ -1,4 +1,5 @@
-import json from '../artifacts/contracts/PastelTokenVendor.sol/Vendor.json' assert { type: 'json' }
+import tokenABI from '../artifacts/contracts/PastelToken.sol/PastelToken.json' assert { type: 'json' }
+import vendorABI from '../artifacts/contracts/PastelTokenVendor.sol/Vendor.json' assert { type: 'json' }
 import { AbiItem, cmd, conversion, Web3 } from './deps.ts'
 
 const program = new cmd.Command('exchanger')
@@ -25,7 +26,7 @@ const buy = async (pastelString: string) => {
   try {
     const accounts = await web3.eth.getAccounts()
     const vendor = new web3.eth.Contract(
-      json.abi as AbiItem[],
+      vendorABI.abi as AbiItem[],
       Deno.env.get('VENDOR_CONTRACT_ADDRESS')
     )
 
@@ -33,7 +34,43 @@ const buy = async (pastelString: string) => {
       from: accounts[0],
       value: web3.utils.toWei(`${ether}`, 'ether'),
     })
-    console.log('You have successfully purchased PASTEL tokens')
+    console.log('You have successfully bought PASTEL tokens')
+    console.log(`Request:`)
+    console.log(request)
+  } catch (exception) {
+    console.error(`Error buying PASTEL: ${exception}`)
+  }
+}
+
+const sell = async (pastelString: string) => {
+  const pastel = Number(pastelString)
+  if (isNaN(pastel)) {
+    console.error('ERROR: The PASTEL quantity must be a number')
+    return
+  }
+
+  try {
+    const accounts = await web3.eth.getAccounts()
+    const tokenContract = new web3.eth.Contract(
+      tokenABI.abi as AbiItem[],
+      Deno.env.get('TOKEN_CONTRACT_ADDRESS')
+    )
+    // Approve the contract to spend the tokens
+    let request = await tokenContract.methods
+      .approve(
+        Deno.env.get('VENDOR_CONTRACT_ADDRESS'),
+        web3.utils.toWei(`${pastel}`, 'ether')
+      )
+      .send({ from: accounts[0] })
+    // Trigger the selling of tokens
+    const vendor = new web3.eth.Contract(
+      vendorABI.abi as AbiItem[],
+      Deno.env.get('VENDOR_CONTRACT_ADDRESS')
+    )
+    request = await vendor.methods
+      .sellTokens(web3.utils.toWei(`${pastel}`, 'ether'))
+      .send({ from: accounts[0] })
+    console.log('You have successfully sold PASTEL tokens')
     console.log(`Request:`)
     console.log(request)
   } catch (exception) {
@@ -45,5 +82,9 @@ program
   .command('buy <pastel>')
   .description('buy PASTEL', { pastel: 'the quantity of PASTEL to buy' })
   .action(buy)
+program
+  .command('sell <pastel>')
+  .description('sell PASTEL', { pastel: 'the quantity of PASTEL to sell' })
+  .action(sell)
 
 program.parse(Deno.args)
